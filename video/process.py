12fc173e0200a6a628db2886c426c775a61f5028
liuyuan-pal <liuyuanwhuer@gmail.com>
2022-03-28 23:42:35 +0800

    update

--- video/process.py
+++
@@ -1,5 +0,0 @@
-import os
-fns=os.listdir('./')
-for fn in fns:
-    if fn.endswith('llff_final.mp4'):
-        os.system(fr'C:\Users\liuyuan\Desktop\eval\bin\ffmpeg.exe -i .\{fn} -vcodec libx264 -crf 24 compress\{fn}')
